76d1279233d76374bd28eb92bb774d72696247eb
Hh440 <harsh100045@gmail.com>
2024-09-04 10:50:09 +0530

    changes

---
+++ index.js
@@ -0,0 +1,13 @@
+const jsonServer = require('json-server')
+
+const server= jsonServer.create()
+const router= jsonServer.router('MetaData.json')
+const middleware= jsonServer.defaults()
+
+const port = process.env.PORT||8080
+
+server.use(middleware)
+
+server.use(router)
+
+server.listen(port)
